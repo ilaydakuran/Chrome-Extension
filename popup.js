5ad55b251ca85a221e21c2a0633bec9d18b6a49c
ilaydakuran <ilaydakuran@sabanciuniv.edu>
2022-03-17 15:54:10 +0300

    first commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener((tab) => {
+    if(!tab.url.includes("chrome://")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+      });
+    }
+  });
---
+++ src/bg/background.js
@@ -0,0 +1,38 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+//example of using a message handler from the inject scripts
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+  });
+  function openTab(){
+    return function(info, tab){
+        let text = info.selectionText;
+        let instalink = "https://twitter.com/explore"; //sağ click yaptıktan sonra direkt bu sayfayı açıyor
+        chrome.tabs.create ({index: tab.index + 1, url: instalink, 
+        selected: true});
+       
+    }
+};
+  var contextMenuItem = {
+    "id": "indir",
+    "title" : " Misogynistic Language Detector", //herhangi bir websitesinde sağ tık yapınca bu görünüyor 
+    "onclick": openTab(),
+  
+  };
+  
+  chrome.contextMenus.create(contextMenuItem);
+
+  chrome.contextMenus.onClicked.addListener(function(clickData){ 
+    alert("Let's eliminate hostility towards women!!!"); //tıkladıktan sonra alert veriliyor ekranda
+    console.log('website is loaded');
+  
+      });
+      
+     
+     
