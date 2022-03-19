ce6ae88d84c66be83a2ef3255dd53b2355a35f86
ilaydakuran <ilaydakuran@sabanciuniv.edu>
2022-03-19 17:26:24 +0300

    insta hashtag sayfasına gidiliyor

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-chrome.action.onClicked.addListener((tab) => {
-    if(!tab.url.includes("chrome://")) {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: reddenPage
-      });
-    }
-  });
+//const activate = document.getElementById('activate');
+
+document.getElementById('automate-form').onsubmit = event => {
+  event.preventDefault();
+  const instatag = document.getElementById("instatag").value;
+  chrome.tabs.create({url: `https://instagram.com/explore/tags/${instatag}`}); 
+  console.log('instada ilgili hashtag kısmına gidildi');
+}
