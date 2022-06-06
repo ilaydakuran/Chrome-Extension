8a4ad4034e27bef962fbb5e626d60458f8f7d5b2
Bekir Kaan Aşcı <kaanasci@Bekir-MacBook-Air.local>
2022-06-06 16:08:00 +0300

    Users can censor multiple words

--- options.js
+++ options.js
@@ -1,7 +1,12 @@
+let word;
+chrome.storage.sync.get(['word'], function(result) {
+  console.log('Value currently is ' + result.word);
+  word = result.word;
+});
 $(function(){
   $('#words').click(function(){
     // console.log($('#word').val());
-    chrome.storage.sync.set({'word': $('#word').val()}, function() {
+    chrome.storage.sync.set({'word': [word, $('#word').val()]}, function() {
       console.log('Value is set to ' + $('#word').val());
     });
   })
